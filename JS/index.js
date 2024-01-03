4b7394f208bae6b39695e6ecf0d18b81be383ed4
g-tapia <gtapia.2400@gmail.com>
2024-01-02 23:43:50 -0600

    removed javascript code for new feature

--- JS/index.js
+++ JS/index.js
@@ -4,7 +4,7 @@ const pc = particlesCursor({
   el: document.getElementById('particle-animation'),
   gpgpuSize: 512, // Increased particle density
   colors: [0xf403dc, 0xf403dc, 0xf403dc], // Three similar shades
-  color: 0xf403dc, // The exact color you provided  
+  color: 0xf403dc, 
   coordScale: .9,
   noiseIntensity: 0.001,
   noiseTimeCoef: 0.0001,
@@ -27,33 +27,4 @@ document.body.addEventListener('click', () => {
   pc.uniforms.uPointSize.value = 1 + Math.random() * 10
 });
 
-// document.body.addEventListener('mousemove', (e) => {
-//   pc.uniforms.uColor.value.setHSL(darkPurpleHue, darkPurpleSaturation, darkPurpleLightness);
-// });
-
-
-// transition js
-
-document.addEventListener('DOMContentLoaded', function () {
-  const projectsLink = document.querySelector('a[href="/portfolio/projects.html"]');
-  const effectContainer = document.getElementById('slash-effect');
-
-  projectsLink.addEventListener('click', function (e) {
-    e.preventDefault();
-    effectContainer.style.display = 'flex';
-
-    // Trigger the slash opening
-    effectContainer.classList.add('active');
-    
-    // After a timeout, open the portal (the purple aura)
-    setTimeout(function () {
-      effectContainer.classList.add('zoomed');
-    }, 500); // The time it takes for the slash to open up
-
-    // After the portal has fully opened, redirect to the projects page
-    setTimeout(function () {
-      window.location.href = '/portfolio/projects.html';
-    }, 1500); // The time it takes to zoom into the rift
-  });
-});
 
